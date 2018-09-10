ddeb9c4cda1407a40836ff1f576eb81965e550fc
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2018-09-10 23:05:05 +0800

    Create 1048@.cpp

---
+++ Advanced Level/1048@.cpp
@@ -0,0 +1,25 @@
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int N = 1005;
+int hashTable[N];
+
+int main() {
+	int n, m, a;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		++hashTable[a];
+	}
+	for (int i = 1; i < m; i++) {
+		if (hashTable[i] && hashTable[m - i]) {
+			if (i == m - 1 && hashTable[i] <= 1) {
+				continue;
+			}
+			printf("%d %d\n", i, m - i);
+			return 0;
+		}
+	}
+	printf("No Solution\n");
+	return 0;
+}
