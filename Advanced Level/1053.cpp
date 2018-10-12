10d2ca947fb804399a6f9da65cac720edafabc0f
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2018-10-12 17:13:27 +0800

    Update 1053.cpp

--- Advanced Level/1053.cpp
+++ Advanced Level/1053.cpp
@@ -1,58 +1,62 @@
 #include<cstdio>
-#include<vector>
 #include<algorithm>
+#include<vector>
 using namespace std;
 const int maxn = 110;
-struct node {
+struct Node {
 	int weight;
 	vector<int> child;
-}Node[maxn];
+}node[maxn];
 int n, m, s;
-int path[maxn];
-
 bool cmp(int a, int b) {
-	return Node[a].weight > Node[b].weight;
+	return node[a].weight > node[b].weight;
 }
-
-void DFS(int index, int numNode, int sum) {
-	if (sum > s)return;
-	if (sum == s) {
-		if (Node[index].child.size() != 0)return;
-		for (int i = 0; i < numNode; i++) {
-			printf("%d", Node[path[i]].weight);
-			if (i < numNode- 1)printf(" ");
-			else {
-				printf("\n");
-			}
+int path[maxn];    //记录每一次路径，用数组可以一遍遍覆盖，最后输出num长度的结果
+void DFS(int index, int num,int w) {              //树的遍历常用参数哦~index, num, w
+	if (w < s) {
+		for (int i = 0; i < node[index].child.size(); i++) {
+			int child = node[index].child[i];
+			path[num] = child;
+			DFS(node[index].child[i],num+1, w + node[node[index].child[i]].weight);
 		}
+	}
+	else if (w > s) {
 		return;
 	}
-	for (int i = 0; i < Node[index].child.size(); i++) {
-		int child = Node[index].child[i];
-		path[numNode] = child;
-		DFS(child, numNode + 1, sum + Node[child].weight);
+	else if (w == s) {
+		if (node[index].child.size() != 0) {
+			return;
+		}
+		else {		
+			for (int j = 0; j < num; j++) {
+				printf("%d", node[path[j]].weight);
+				if (j < num - 1) {
+					printf(" ");
+				}
+				else {
+					printf("\n");
+				}
+			}
+			return;
+		}
 	}
 }
-
-
-
 int main() {
+	int w, id, k,cid;
 	scanf("%d%d%d", &n, &m, &s);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &Node[i].weight);
+		scanf("%d", &w);
+		node[i].weight = w;
 	}
-	int id,k,child;
 	for (int i = 0; i < m; i++) {
-		
 		scanf("%d%d", &id, &k);
-		for (int i = 0; i < k; i++) {
-			scanf("%d", &child);
-			Node[id].child.push_back(child);
+		for (int j = 0; j < k; j++) {
+			scanf("%d", &cid);
+			node[id].child.push_back(cid);
 		}
-		sort(Node[id].child.begin(), Node[id].child.end(), cmp);
+		sort(node[id].child.begin(), node[id].child.end(), cmp);
 	}
 	path[0] = 0;
-	DFS(0, 1, Node[0].weight);
+	DFS(0,1,node[0].weight);
 	return 0;
 }
-
