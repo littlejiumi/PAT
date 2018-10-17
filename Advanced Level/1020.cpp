fba188cb5e6fffb61093df05bd9f7c235cd8fd7f
Xiaohan Zhang <36367749+littlejiumi@users.noreply.github.com>
2018-10-17 21:23:08 +0800

    Update 1020.cpp

--- Advanced Level/1020.cpp
+++ Advanced Level/1020.cpp
@@ -1,54 +1,53 @@
 #include<cstdio>
-#include<cstring>
-#include<queue>
 #include<algorithm>
+#include<queue>
 using namespace std;
-
-const int maxn = 50;
-
+int n;
+const int maxn = 35;
+int post[maxn], in[maxn];
 struct node {
 	int data;
 	node* lchild;
 	node* rchild;
 };
-
-int in[maxn], post[maxn];
-int n;
-
-node* create(int postL, int postR, int inL, int inR) {
-	if (postL > postR) {
-		return NULL;
+node* create(int postl, int postr,int inl,int inr) { //根据后序和中序建树
+	if (postl > postr) {
+		return NULL;       //不是return
 	}
-	node* root = new node;
-	root->data = post[postR];
 	int k;
-	for ( k = inL; k <= inR; k++) {
-		if (in[k] == post[postR]) {
+	for (int i = inl; i<=inr; i++) {
+		if (in[i] == post[postr]) {
+			k = i;
 			break;
 		}
 	}
-	int numLeft = k - inL;
-	root->lchild = create(postL, postL + numLeft - 1, inL, k - 1);
-	root->rchild = create(postL + numLeft, postR - 1, k + 1, inR);
+	int leftnum = k - inl;
+	node* root = new node;       //根节点data是后续最后一个
+	root->data = post[postr];
+	root->lchild = create(postl, postl + leftnum - 1, inl, k - 1);
+	root->rchild = create(postl + leftnum, postr-1, k + 1, inr);
 	return root;
 }
-
 int num = 0;
-
-void BFS(node* root) {
-	queue<node*> q;
+void BFS(node* root) {       //建树后层序遍历，注意要计数加空格
+	queue<node*> q;             //存地址
 	q.push(root);
 	while (!q.empty()) {
-		node* now = q.front();
-		q.pop();
-		printf("%d", now->data);
+		node* front = q.front();
+		printf("%d", front->data);
 		num++;
-		if (num < n) printf(" ");
-		if (now->lchild != NULL) q.push(now->lchild);
-		if (now->rchild != NULL) q.push(now->rchild);
+		if (num < n) {
+			printf(" ");
+		}
+		q.pop();
+		if (front->lchild != NULL) {
+			q.push(front->lchild);
+		}
+		if (front->rchild != NULL) {
+			q.push(front->rchild);
+		}
 	}
 }
-
 int main() {
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
@@ -57,7 +56,7 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &in[i]);
 	}
-	node* root = create(0, n - 1, 0, n - 1);
+	node* root = create(0, n - 1, 0, n - 1); //起始和终了下标， 返回所得树的根结点
 	BFS(root);
 	return 0;
 }
